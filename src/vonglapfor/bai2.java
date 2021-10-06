bee746fef4743b7d65b376ece82200b78f6317bc
truongcaolop3 <truongcaolop3@gmail.com>
2021-10-06 16:43:56 +0700

    init phien1bai2

---
+++ src/vonglapfor/bai2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vonglapfor;
+
+/**
+ *
+ * @author DELL
+ */
+public class bai2 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        for (int i= 5 ; i>=0;i--){
+            for (int j =i ; j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
