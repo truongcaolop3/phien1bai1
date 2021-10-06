23ffa79b277bb4749b83390a1000fcdc7fc7f373
truongcaolop3 <truongcaolop3@gmail.com>
2021-10-06 16:59:30 +0700

    init phien1bai4

---
+++ src/vonglapfor/baii5.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vonglapfor;
+
+/**
+ *
+ * @author DELL
+ */
+public class baii5 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int n;
+ 
+        for ( int i=-10;i<0;i++)
+            System.out.println(i);
+    }
+}
