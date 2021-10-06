58a15a330e2445d64a4c7b5d4ab4a6d3187b02f6
truongcaolop3 <truongcaolop3@gmail.com>
2021-10-06 16:31:20 +0700

    init phien1

---
+++ src/vonglapfor/bai1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vonglapfor;
+
+/**
+ *
+ * @author DELL
+ */
+public class bai1 {
+
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String[] args) {
+        // TODO code application logic here
+        
+//        Scanner sc = new Scanner(System.in);
+        int n =10;
+//        n = sc.nextInt();
+        
+        for (int i = 1 ; i<=10;i++)
+            System.out.println(i);
+    }
+    
+}
